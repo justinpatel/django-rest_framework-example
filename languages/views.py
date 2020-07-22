567b07761d595e0ec20553e09f493d596b338b1f
Justin Patel <pateljustin428@gmail.com>
2020-07-22 13:00:30 +0530

    first commit

---
+++ languages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .serializers import LanguageSerializer
+from .models import Language
+from rest_framework import viewsets
+
+class LanguageView(viewsets.ModelViewSet):
+    queryset = Language.objects.all()
+    serializer_class = LanguageSerializer
